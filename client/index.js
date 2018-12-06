f937af6d5ce2fb2e76e4b7870478efca9f1ea6c5
FCraven <craven.francis@gmail.com>
2018-12-05 23:53:44 -0500

    added axios call to componentDidMount

--- client/index.js
+++ client/index.js
@@ -1,22 +1,32 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
 import ContactList from './ContactList'
 
-const contacts = [
-  {"id": 1, "name": "R2-D2", "phone": "222-222-2222", "email": "r2d2@droids.com"},
-  {"id": 2, "name": "C-3PO", "phone": "333-333-3333", "email": "c3po@droids.com"},
-  {"id": 3, "name": "BB-8", "phone": "888-888-8888", "email": "bb8@droids.com"}
-]
+
+
 
 class Main extends Component {
   constructor() {
     super()
     this.state = {
-      contacts: contacts
+      contacts: []
     }
   }
 
-  render () {
+  async componentDidMount(){
+    try {
+      const res = await axios.get('/api/contacts')
+      const contacts = res.data
+      this.setState({
+        contacts: contacts
+      })
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
+  render() {
     return (
       <div id='main'>
         <div id='navbar'>
